7e4d64e905462814ecd866a1e75736c073b8f078
Rizwan Ali <rizwandiplana@gmail.com>
2023-08-02 11:11:59 +0500

    Email changes

--- booking-complete.php
+++ booking-complete.php
@@ -1,4 +1,11 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+
 
     //learn from w3schools.com
 
@@ -24,6 +31,83 @@
     // $username=$userfetch["pname"];
     if($_POST){
         if(isset($_POST["booknow"])){
+            $mail = new PHPMailer();
+
+            $mail->isSMTP();
+            $mail->Host= 'smtp.gmail.com';
+            $mail->SMTPAuth= 'true';
+            $mail->Username= 'dermadenlaserclinic@gmail.com';
+            $mail->Password= 'yqmnqddpsiihxfaj';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+
+            $mail->setFrom('dermadenlaserclinic@gmail.com');
+            $mail->addAddress("dermadenlaserclinic@gmail.com");
+            // $mail->isHTML(true);
+            $mail->Subject = 'Dermaden Laser Clinic - New Appointment Booked for ' . $_POST["type"] . '';
+            $mail->Body = '
+            Hey!
+
+            It is to notify you that an appointment has been booked with ' . $_POST["name"] . ' for ' . $_POST["date"] . ' at ' . $_POST["time"] . ' EST (GMT -4). 
+            
+            Contact No.: ' . $_POST["cell"] .
+
+            '
+            
+            This is an automatically generated email. Please do not reply to this email.
+
+            Dermaden Laser Clinic
+            ';
+
+            // Send the email
+            if ($mail->send()) {
+                echo 'Email sent successfully!';
+                // return;
+            } else {
+                echo 'Error sending email. Error message: ' . $mail->ErrorInfo;
+                // return;
+            }
+
+
+            $mail1 = new PHPMailer();
+
+            $mail1->isSMTP();
+            $mail1->Host= 'smtp.gmail.com';
+            $mail1->SMTPAuth= 'true';
+            $mail1->Username= 'dermadenlaserclinic@gmail.com';
+            $mail1->Password= 'yqmnqddpsiihxfaj';
+            $mail1->SMTPSecure = 'ssl';
+            $mail1->Port = 465;
+
+            $mail1->setFrom('dermadenlaserclinic@gmail.com');
+            $mail1->addAddress($_POST["email"]);
+            // $mail->isHTML(true);
+            $mail1->Subject = 'Dermaden Laser Clinic - Appointment Scheduled for ' . $_POST["type"] . '';
+            $mail1->Body = '
+            Hey!
+
+            Thankyou for showing interest in Dermaden Laser Clinic.
+            
+            It is to notify you that your appointment has been booked with our skin care specialists for ' . $_POST["date"] . ' at ' . $_POST["time"] . ' EST (GMT -4). 
+            
+            We hope to see you soon!
+            
+            This is an automatically generated email. Please do not reply to this email.
+
+            Dermaden Laser Clinic
+            ';
+
+            // Send the email
+            if ($mail1->send()) {
+                echo 'Email sent successfully!';
+                // return;
+            } else {
+                echo 'Error sending email. Error message: ' . $mail->ErrorInfo;
+                // return;
+            }
+
+
+ 
             // $apponum=3;
             // $scheduleid=$_POST["scheduleid"];
             $name=$_POST["name"];
